05c223a99e28833b8bdfdc79ff7c18868cebf8bc
Jsatchell03 <jsatchell2017@gmail.com>
2025-09-10 15:48:58 -0400

    choices.json replaced with const in Questionnaire component

--- src/components/Questionnaire.jsx
+++ src/components/Questionnaire.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { useState } from "react";
 
+const choices = {
+  yesNo: ["Yes", "No"],
+  states: [],
+  countries: [],
+  nonImmigrantVisas: [],
+  immigrantVisas: [],
+};
+
 export default function Questionnaire() {
   const [currQuestion, setCurrQuestion] = useState(0);
 
